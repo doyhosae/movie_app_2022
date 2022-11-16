9d1a6eeb377f523de6475e76fb2b96cf476d6c1f
doyhosae <doyhosae@gmail.com>
2022-11-16 18:50:05 +0900

    06-3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-function Food(props) {
-  return <h1>I like { props.fav }</h1>;
-}
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
+  }
 
-function App() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-      <Food fav="kimchi" />
-      <Food fav="samgiopsal" />
-    </div>
-  );
+  componentDidMount() {
+    // 영화 데이터 로딩
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? 'Loading...'
+          : movies.map((movie) => {
+            console.log(movie);
+            return <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+            />;
+          })}
+      </div>
+    );
+  }
 }
 
 export default App;
